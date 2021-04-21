9357da1f14dd4a51f828d20facee41d8a21882e9
Armagan Dalkiran <armagandalkran20@gmail.com>
2021-04-21 10:14:20 +0300

    21.04.2021
    First node.js attemp with using .env

--- first_try.js
+++ first_try.js
@@ -1,14 +1,20 @@
+require('dotenv').config()
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = process.env.HOST
+const port = process.env.PORT
+// const hostname = '127.0.0.1';
+// const port = 3000;
+const city = process.env.CITY
+const name = process.env.NAME
+const age = process.env.AGE
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+     res.statusCode = 200;
+     res.setHeader('Content-Type', 'text/plain');
+     res.end(`${city},${age},${name}`);
+   });
+   
+   server.listen(port, hostname, () => {
+     console.log(`Server running at http://${hostname}:${port}/`);
+   });
